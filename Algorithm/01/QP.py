9bbfa3ec17c07bfe2b1ac9c5d769a6106daed2c9
tzh <zhihuatean@gmail.com>
2023-09-14 07:49:36 +0000

    change the psd m

--- Algorithm/01/QP.py
+++ Algorithm/01/QP.py
@@ -7,19 +7,23 @@ from gurobi_init import e
 if __name__ == '__main__':
     model = gp.Model(env=e, name='QP')
     
-    x = model.addVar(lb=0, vtype=GRB.CONTINUOUS, name='x')
-    y = model.addVar(lb=0, vtype=GRB.CONTINUOUS, name='x')
-    z = model.addVar(lb=0, vtype=GRB.CONTINUOUS, name='x')
+    x = model.addVar(lb=0, vtype=GRB.BINARY, name='x')
+    y = model.addVar(lb=0, vtype=GRB.BINARY, name='y')
+    z = model.addVar(lb=0, vtype=GRB.BINARY, name='z')
     
-    model.setObjective(x * x + y * z)
+    model.setObjective(x * x + y * z, GRB.MAXIMIZE)
     
     quad_expr = gp.QuadExpr()
     
+    quad_expr.addTerms(1, x, x)
     quad_expr.addTerms(1, x, y)
     quad_expr.addTerms(1, y, z)
 
     model.addQConstr(quad_expr <= z + 5)
-    model.addConstr(x + z <= 6)
+    model.addConstr(3 * x + 2 * z <= 4)
     
     model.optimize()
+    print('x = ', x.x)
+    print('y = ', y.x)
+    print('z = ', z.x) 
     
