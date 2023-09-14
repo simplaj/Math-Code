4dec23826e14953cf04de08704b4cc0dd8e945ca
tzh <zhihuatean@gmail.com>
2023-09-14 07:45:28 +0000

    realize qp use gurobi

--- Algorithm/01/QP.py
+++ Algorithm/01/QP.py
@@ -1,13 +1,25 @@
-from cvxopt import matrix, solvers
+import gurobipy as gp
+from gurobipy import GRB
 
-# 定义二次规划的系数矩阵和向量
-P = matrix([[1.0, 0.4], [0.4, 1.0]])
-q = matrix([-1.0, -2.0])
+from gurobi_init import e
 
-# 定义约束条件的系数矩阵和右侧常数向量
-G = matrix([[-1.0, 0.0], [0.0, -1.0]])
-h = matrix([0.0, 0.0])
 
-# 求解二次规划问题
-sol = solvers.qp(P, q, G, h)
-print(sol['x'])
+if __name__ == '__main__':
+    model = gp.Model(env=e, name='QP')
+    
+    x = model.addVar(lb=0, vtype=GRB.CONTINUOUS, name='x')
+    y = model.addVar(lb=0, vtype=GRB.CONTINUOUS, name='x')
+    z = model.addVar(lb=0, vtype=GRB.CONTINUOUS, name='x')
+    
+    model.setObjective(x * x + y * z)
+    
+    quad_expr = gp.QuadExpr()
+    
+    quad_expr.addTerms(1, x, y)
+    quad_expr.addTerms(1, y, z)
+
+    model.addQConstr(quad_expr <= z + 5)
+    model.addConstr(x + z <= 6)
+    
+    model.optimize()
+    
